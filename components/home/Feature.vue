e152db9cb05dbb2e75f0e7f2dd4d49ce39a72bd4
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2022-07-02 09:33:05 +0100

    updates

--- components/home/Feature.vue
+++ components/home/Feature.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="bg-white min-h-screen text-center h-[400vh]" ref="targetSection">
-    <div class="container flex flex-col items-center justify-center space-y-6 lg:space-y-9 top-0 sticky min-h-screen overflow-hidden">
+    <div class="container flex flex-col items-center justify-center top-0 sticky min-h-screen overflow-hidden">
       <div
         class="lg:text-lg text-primary font-gilmer uppercase border-2 border-[#e3e3e3] rounded-full tracking-widest px-4 lg:px-6 py-4 leading-none">
         features <span class="drop-shadow-lg ml-2">🚀</span>
       </div>
 
       <!-- Titles -->
-      <div class="flex flex-col items-center">
-        <div class="font-title text-5xl lg:text-7xl font-black leading-tight w-max relative overflow-hidden">
+      <div class="flex flex-col items-center mt-10 mb-8 space-y-2">
+        <div class="font-title text-5xl lg:text-8xl font-black leading-tight w-max relative overflow-hidden">
           <Presence :exit-before-enter="true">
             <template v-for="(titles, index) in allTitles" :key="index">
               <Motion :initial="titleTextSwitcherAnimation.initial" :animate="titleTextSwitcherAnimation.animate"
@@ -20,7 +20,7 @@
           </Presence>
         </div>
 
-        <div class="font-title text-5xl lg:text-7xl font-black leading-tight w-max relative overflow-hidden">
+        <div class="font-title text-5xl lg:text-8xl font-black leading-tight w-max relative overflow-hidden">
           <Presence :exit-before-enter="true">
             <template v-for="(titles, index) in allTitles" :key="index">
               <Motion :initial="titleTextSwitcherAnimation.initial" :animate="titleTextSwitcherAnimation.animate"
@@ -32,7 +32,7 @@
           </Presence>
         </div>
 
-        <div class="font-title text-5xl lg:text-7xl font-black leading-tight w-max relative overflow-hidden">
+        <div class="font-title text-5xl lg:text-8xl font-black leading-tight w-max relative overflow-hidden">
           <Presence :exit-before-enter="true">
             <template v-for="(titles, index) in allTitles" :key="index">
               <Motion :initial="titleTextSwitcherAnimation.initial" :animate="titleTextSwitcherAnimation.animate"
@@ -48,21 +48,24 @@
       <!-- Description -->
       <Presence :exit-before-enter="true">
         <template v-for="(description, index) in allDescriptions" :key="index+description">
-          <Motion tag="p" class="text-gray-400 font-gilmer text-base lg:text-xl max-w-xl pb-5 lg:-mt-4" v-if="activeFeatureIndex === index"
-            :initial="descriptionTextSwitcherAnimation.initial" :animate="descriptionTextSwitcherAnimation.animate"
-            :exit="descriptionTextSwitcherAnimation.exit" :transition="descriptionTextSwitcherAnimation.transition">
+          <Motion tag="p" class="text-gray-400 font-gilmer text-base lg:text-xl max-w-xl pb-5 lg:-mt-4"
+            v-if="activeFeatureIndex === index" :initial="descriptionTextSwitcherAnimation.initial"
+            :animate="descriptionTextSwitcherAnimation.animate" :exit="descriptionTextSwitcherAnimation.exit"
+            :transition="descriptionTextSwitcherAnimation.transition">
             {{ description.description }}
           </Motion>
         </template>
       </Presence>
 
       <!-- Actions -->
-      <NuxtLink :href="activeFeature?.action" :target="activeFeature?.actionType === 'internal' ? '' : '__blank'"
-        v-if="activeFeature?.hasAction"
-        class="border-2 p-4 lg:p-6 rounded-xl border-[#e3e3e3] text-black lg:text-lg font-semibold leading-none flex items-center transition duration-200 hover:border-primary hover:border-opacity-40">
-        <span class="font-gilmer">Find Out More</span>
-        <ArrowRightIcon class="w-5 text-primary ml-2 -rotate-45"></ArrowRightIcon>
-      </NuxtLink>
+      <div class="mt-10 mb-16">
+        <NuxtLink :href="activeFeature?.action" :target="activeFeature?.actionType === 'internal' ? '' : '__blank'"
+          v-if="activeFeature?.hasAction"
+          class="border-2 p-4 lg:p-5 rounded-xl border-[#e3e3e3] text-black lg:text-lg font-semibold leading-none flex items-center transition duration-200 hover:border-primary hover:border-opacity-40">
+          <span class="font-gilmer">Find Out More</span>
+          <ArrowRightIcon class="w-5 text-primary ml-2 -rotate-45"></ArrowRightIcon>
+        </NuxtLink>
+      </div>
 
       <!-- Feature Images -->
       <div class="flex justify-around">
@@ -71,8 +74,7 @@
             <template v-for="(image, index) in allImages" :key="index">
               <Motion :initial="imageSwitcherAnimation.initial" :animate="imageSwitcherAnimation.animate"
                 :exit="imageSwitcherAnimation.exit" :transition="imageSwitcherAnimation.transition"
-                v-if="activeFeatureIndex === index" tag="img" :src="image.imageOne"
-                class="w-full h-full object-cover">
+                v-if="activeFeatureIndex === index" tag="img" :src="image.imageOne" class="w-full h-full object-cover">
               </Motion>
             </template>
           </Presence>
@@ -83,8 +85,7 @@
             <template v-for="(image, index) in allImages" :key="index">
               <Motion :initial="imageSwitcherAnimation.initial" :animate="imageSwitcherAnimation.animate"
                 :exit="imageSwitcherAnimation.exit" :transition="imageSwitcherAnimation.transition"
-                v-if="activeFeatureIndex === index" tag="img" :src="image.imageTwo"
-                class="w-full h-full object-cover">
+                v-if="activeFeatureIndex === index" tag="img" :src="image.imageTwo" class="w-full h-full object-cover">
               </Motion>
             </template>
           </Presence>
@@ -107,12 +108,16 @@
             <template v-for="(image, index) in allImages" :key="index">
               <Motion :initial="imageSwitcherAnimation.initial" :animate="imageSwitcherAnimation.animate"
                 :exit="imageSwitcherAnimation.exit" :transition="imageSwitcherAnimation.transition"
-                v-if="activeFeatureIndex === index" tag="img" :src="image.imageFour"
-                class="w-full h-full object-cover">
+                v-if="activeFeatureIndex === index" tag="img" :src="image.imageFour" class="w-full h-full object-cover">
               </Motion>
             </template>
           </Presence>
         </div>
+      </div>
+
+      <div class="flex items-center flex-col justify-center">
+        <SharedScrollProgress :scroll-progress="mouseIndicatorScrollProgress"></SharedScrollProgress>
+        <p class="font-gilmer font-semibold text-xs mt-4 leading-none">Scroll Down</p>
       </div>
     </div>
   </section>
@@ -129,6 +134,7 @@ const targetSection = ref<HTMLElement>(null)
 
 const activeFeatureIndex = ref(0)
 const activeFeature = computed(() => featuredFeatures[activeFeatureIndex.value])
+const mouseIndicatorScrollProgress = ref(1 / featuredFeatures.length)
 
 const allFeatures = computed(() => featuredFeatures)
 const allTitles = computed(() => featuredFeatures.map(({ titleOne, titleTwo, titleThree }) => ({ titleOne, titleTwo, titleThree })))
@@ -165,6 +171,10 @@ const initiateAnimationTrigger = () => {
       if (index === activeFeatureIndex.value) true
       else activeFeatureIndex.value = index
     }
+  })
+
+  watch(activeFeatureIndex, () => {
+    mouseIndicatorScrollProgress.value = (activeFeatureIndex.value + 1) / allFeatures.value.length
   })
 }
 
