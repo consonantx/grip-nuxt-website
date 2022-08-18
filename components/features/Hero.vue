4fd912995d07aca2b1116d3729fdb5c711ebe391
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2022-08-18 10:49:33 +0100

    updates

--- components/features/Hero.vue
+++ components/features/Hero.vue
@@ -3,7 +3,7 @@
     <div class="container flex flex-col items-center justify-center top-0 sticky overflow-hidden">
       <!-- Titles -->
       <div class="flex flex-col items-center mt-4 lg:mt-10 mb-3 lg:mb-6 lg:space-y-2">
-        <div class="font-title text-5xl lg:text-6xl xl:text-8xl font-black leading-tight w-max relative overflow-hidden">
+        <div class="font-title text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-black leading-tight w-max relative overflow-hidden">
           <Presence :exit-before-enter="true">
             <Motion :initial="titleTextSwitcherAnimation.initial" :animate="titleTextSwitcherAnimation.animate"
               :exit="titleTextSwitcherAnimation.exit" :transition="titleTextSwitcherAnimation.transition">
@@ -14,8 +14,8 @@
       </div>
 
       <!-- Titles -->
-      <div class="flex flex-col items-center mt-2 mb-3 lg:mb-16 lg:space-y-2">
-        <div class="font-title text-5xl lg:text-6xl xl:text-8xl font-black leading-tight w-max relative overflow-hidden">
+      <div class="flex flex-col items-center mt-2 mb-10 lg:mb-16 lg:space-y-2">
+        <div class="font-title text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-black leading-tight w-max relative overflow-hidden">
           <Presence :exit-before-enter="true">
             <Motion :initial="titleTextSwitcherAnimation.initial" :animate="titleTextSwitcherAnimation.animate"
               :exit="titleTextSwitcherAnimation.exit" :transition="titleTextSwitcherAnimation.transition">
@@ -84,7 +84,7 @@
         </div>
       </div>
 
-      <div class="flex items-center flex-col justify-center pt-24 pb-10">
+      <div class="flex items-center flex-col justify-center pt-20 pb-6 lg:pb-10">
         <div class="flex justify-center lg:justify-end -translate-y-10 lg:-translate-y-12">
           <button class="flex flex-col items-center" @click="scrollTo(500)">
             <ArrowCircleDownIcon class="w-10 lg:w-14 mb-3 stroke-1 animate-bounce duration-1000"></ArrowCircleDownIcon>
