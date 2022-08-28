e0d79fc64706d888e9114ee1cb8fc2b280eaad1d
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2022-08-28 21:17:46 +0100

    chore: update 404 page image

--- error.vue
+++ error.vue
@@ -5,19 +5,20 @@
       <div class="flex flex-col-reverse md:flex-row flex-grow mt-16 lg:mt-28">
         <div class="flex flex-col flex-grow space-y-12 mt-28 lg:mt-0">
           <h1 class="text-4xl lg:text-6xl font-black font-title break-words">
-            Something's <br/>
+            Something's <br />
             Missing
           </h1>
           <button class="px-16 py-4 border-2 w-max rounded-lg font-gilmer" @click="handleError">Go Home</button>
         </div>
         <div class="flex flex-grow -ml-10">
           <img
-            src="https://res.cloudinary.com/consonantafrica/image/upload/v1653244204/website/pos_design_trans_elmpro.png"
-            width="3840" height="2160" class="rotate-[-25deg]"/>
+            src="https://res.cloudinary.com/consonantafrica/image/upload/q_70/v1661717808/website/pos_design_trans_pwcp73.webp"
+            width="3840" height="2160" class="rotate-[-25deg]" />
         </div>
       </div>
     </div>
-    <span class="text-[200px] lg:text-[650px] font-bold text-[#262626] absolute bottom-60 lg:bottom-0 leading-none z-0 font-title">404</span>
+    <span
+      class="text-[200px] lg:text-[650px] font-bold text-[#262626] absolute bottom-60 lg:bottom-0 leading-none z-0 font-title">404</span>
   </div>
 </template>
 
