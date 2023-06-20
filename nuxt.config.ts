d71516cf43cc47a09bef8d6fbb8678d353337b4e
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2023-06-20 12:46:16 +0100

    feat: upgrade nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
-export default defineNuxtConfig({
+export default {
   modules: ['@nuxtjs/tailwindcss'],
   build: {
     transpile: ['motion/vue', '@motionone/vue', '@heroicons/vue'],
   }
-})
+}
